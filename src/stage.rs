e370259957657dd464ea3184a645bd443f0ac7d6
Yuriy D. Sibirmovsky <76599593+YDS67@users.noreply.github.com>
2024-01-06 15:30:47 +0300

    Update stage.rs

--- src/stage.rs
+++ src/stage.rs
@@ -185,7 +185,7 @@ impl Stage {
         };
         let texture_main = ctx.new_texture_from_data_and_format(pixels.as_bytes(), params);
         ctx.texture_generate_mipmaps(texture_main);
-        unsafe{miniquad::gl::glTexParameteri(0, miniquad::gl::GL_TEXTURE_MAX_LEVEL, 5)}
+        //unsafe{miniquad::gl::glTexParameteri(0, miniquad::gl::GL_TEXTURE_MAX_LEVEL, 100)}
 
         let pixels: ImageBuffer<Rgba<u8>, Vec<u8>> = ass.font;
         let dims = pixels.dimensions();
