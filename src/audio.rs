fccd00f487f7684eb0e731d879eaaa17bfbedf02
Yuriy D. Sibirmovsky <76599593+YDS67@users.noreply.github.com>
2024-01-09 02:51:22 +0300

    Update audio.rs

--- src/audio.rs
+++ src/audio.rs
@@ -1,11 +1,16 @@
 use std::io::BufReader;
 
+use rodio::Source;
+
 pub fn playback() {
     let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
     let sink = rodio::Sink::try_new(&handle).unwrap();
 
     let file = std::fs::File::open("assets/music.wav").unwrap();
-    sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
+    let buffer = rodio::Decoder::new(BufReader::new(file)).unwrap().buffered();
 
-    sink.sleep_until_end();
+    loop {
+        sink.append(buffer.clone());
+        sink.sleep_until_end();
+    }
 }
